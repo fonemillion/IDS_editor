bf0648c46c84cbf4e4da3376498d355d74f0d4e0
fonemillion <fonemillion@gmail.com>
2026-07-27 01:04:11 +0200

    1.1

--- src/main.py
+++ src/main.py
@@ -25,6 +25,7 @@ from math import atan2, degrees
 import tkintermapview
 from tkintermapview import TkinterMapView
 
+from ml_euroradar.gpr_reader.gssi_reader import GSSI_Reader
 from ml_euroradar.gpr_reader.ids_reader import IDS_Reader
 
 from shapely.geometry import LineString
@@ -102,7 +103,7 @@ def _gps_line_to_poly(line,work_crs = "EPSG:28992"):
 
 
 def _show_about():
-    messagebox.showinfo("About", "My Professional App\nVersion 1.0")
+    messagebox.showinfo("About", "Quick GPR data checker \nVersion 1.0")
 
 def _exit_app():
     root.destroy()
@@ -116,7 +117,7 @@ def show_welcome():
 
     label = ttk.Label(
         content_frame,
-        text="Open an IDS project to begin",
+        text="Open an IDS or GSSI project to begin",
         font=("Segoe UI", 14)
     )
     label.place(relx=0.5, rely=0.5, anchor="center")
@@ -130,9 +131,18 @@ def _open_ids():
         IDS_reader.load_project(folder)
         status_label.config(text=f"Loaded folder: {folder}")
 
-        show_swaths()
+        show_swaths_ids()
 
+def _open_gssi():
+    folder = filedialog.askdirectory(
+        title="Select IDS Data Folder"
+    )
 
+    if folder:  # User didn't cancel
+        GSSI_reader.load_project(folder)
+        status_label.config(text=f"Loaded folder: {folder}")
+
+        show_swaths_gssi()
 
 # Keep references to the drawn path objects so we can remove/redraw them
 map_paths = []
@@ -186,6 +196,49 @@ def update_plot():
 
         map_paths.append(path)
 
+def update_plot_gssi():
+    # todo cleanup
+    """Redraw the GPS routes on the map based on which swaths are checked."""
+    for path in map_paths:
+        path.delete()
+    map_paths.clear()
+
+    for item in tree.get_children():
+        values = tree.item(item, "values")
+        checked, swath, gnns_string, swat_type, swat_depth = values
+
+        swath = swath + '.DZT'
+        if gnns_string == "☐":
+            continue
+        ###
+        if line_not_poly:
+            if checked == "☐":   # ☐ ☑
+                if item in tree.selection():
+                    path = map_widget.set_path(swath_routes[swath], color="#b1b5ba", width=3)
+                else:
+                    path = map_widget.set_path(swath_routes[swath], color="#adb5bd", width=1)
+            else:
+                if item in tree.selection():
+                    path = map_widget.set_path(swath_routes[swath], color="#f5e022", width=4)
+                else:
+                    path = map_widget.set_path(swath_routes[swath], color="#e03131", width=2)
+
+        else:
+            if checked == "☐":   # ☐ ☑
+                if item in tree.selection():
+                    path = map_widget.set_polygon(swath_poly[swath], outline_color="#b1b5ba", fill_color="#b1b5ba", border_width=3)
+                else:
+                    path = map_widget.set_polygon(swath_poly[swath], outline_color="#adb5bd", fill_color="#adb5bd", border_width=1)
+            else:
+                if item in tree.selection():
+                    path = map_widget.set_polygon(swath_poly[swath], outline_color="#f5e022", fill_color="#f5e022", border_width=4)
+                else:
+                    path = map_widget.set_polygon(swath_poly[swath], outline_color="#e03131", fill_color="#e03131", border_width=2)
+
+
+        map_paths.append(path)
+
+
 def _switch_line_poly():
     global line_not_poly
 
@@ -196,6 +249,17 @@ def _switch_line_poly():
         line_button.config(text="poly")
         line_not_poly = True
     update_plot()
+
+def _switch_line_poly_gssi():
+    global line_not_poly
+
+    if line_not_poly:
+        line_button.config(text="line")
+        line_not_poly = False
+    else:
+        line_button.config(text="poly")
+        line_not_poly = True
+    update_plot_gssi()
 
 def _sort_tl():
     # transformer
@@ -308,7 +372,7 @@ def _switch_map():
     map_toggle = (not map_toggle)
 
 
-def show_swaths():
+def show_swaths_ids():
     clear_content()
 
     # --- Left/right split: tree on the left, plot on the right ---
@@ -463,8 +527,168 @@ def show_swaths():
 
     tree.bind("<<TreeviewSelect>>", on_tree_select)
 
+def show_swaths_gssi():
+    clear_content()
+
+    # --- Left/right split: tree on the left, plot on the right ---
+    paned = ttk.PanedWindow(content_frame, orient=tk.HORIZONTAL)
+    paned.pack(fill=tk.BOTH, expand=True)
+
+    tree_frame = ttk.Frame(paned)
+    plot_frame = ttk.Frame(paned)
+    paned.add(tree_frame, weight=0)
+    paned.add(plot_frame, weight=1)
+
+    # === Toolbar list ===
+    # toolbar_tree = ttk.Frame(tree_frame, padding=(5, 3))
+    # toolbar_tree.pack(side="top", fill="x")
+    #
+    # save_button = ttk.Button(toolbar_tree, text="save", command=_save_svy)
+    # save_button.pack(side="left", padx=2)
+    #
+    # switch_tl_button = ttk.Button(toolbar_tree, text="switch T/L", command=_switch_tl)
+    # switch_tl_button.pack(side="left", padx=2)
+
+
+    # === Toolbar map ===
+    toolbar_map = ttk.Frame(plot_frame, padding=(5, 3))
+    toolbar_map.pack(side="top", fill="x")
+    #
+    global line_not_poly
+    line_not_poly = True
+    global line_button
+    #
+    line_button = ttk.Button(toolbar_map, text="poly", command=_switch_line_poly_gssi)
+    line_button.pack(side="left", padx=2)
+    #
+    # sort_tl_button = ttk.Button(toolbar_map, text="sort T/L", command=_sort_tl)
+    # sort_tl_button.pack(side="left", padx=2)
+    #
+    # switch_map_button = ttk.Button(toolbar_map, text="switch map", command=_switch_map)
+    # switch_map_button.pack(side="left", padx=2)
+
+    # ttk.Button(toolbar, text="Save").pack(side="left", padx=2)
+    # ttk.Button(toolbar, text="KML").pack(side="left", padx=2)
+
+    # --- Tree (left) ---
+    global tree
+    tree = ttk.Treeview(
+        tree_frame,
+        columns=("selected", "swath", "gnns", "type", "depth"),
+        show="headings"
+    )
+
+
+
+    tree.heading("selected", text="")
+    tree.heading("swath", text="Swath")
+    tree.heading("gnns", text="GNNS")
+    tree.heading("type", text="Type")
+    tree.heading("depth", text="Depth")
+
+    # tree.column("selected", width=35, anchor="center", stretch=False)
+    # tree.column("swath", width=50, stretch=False)
+    # tree.column("length", width=100, stretch=False)
+    # tree.column("type", width=50, anchor="center", stretch=False)
+
+    scrollbar = ttk.Scrollbar(
+        tree_frame,
+        orient="vertical",
+        command=tree.yview
+    )
+    tree.configure(yscrollcommand=scrollbar.set)
+
+    tree.pack(side="left", fill=tk.BOTH, expand=True)
+    scrollbar.pack(side="right", fill="y")
+
+    # length_list = IDS_reader.swat_lengths
+    swat_list = []
+    for swat in GSSI_reader.DZT_files:
+        swat_list.append(swat)
+
+
+
+    show_list = []
+    for swat in swat_list:
+        swat_type, data_points =  GSSI_reader.read_header(swat)
+        show_list.append((swat,  (swat[:-4]+".DZG") in GSSI_reader.DZG_files, swat_type, GSSI_reader.get_time_window(swat)))
+
+    # convert to (swath, length_m, "L"/"T") for tree rows
+    for swath, gnns, swat_type, swat_depth in show_list:
+        gnns_string = "☑" if gnns else "☐"
+        tree.insert("", tk.END, values=("☑", swath[:-4], gnns_string, swat_type, str(swat_depth)))
+
+    # --- Map (right) ---
+    global map_widget, swath_routes, swath_poly
+    # swath_routes = generate_mock_routes(rows)
+    swath_routes = dict()
+    for swat in swat_list:
+        if (swat[:-4]+".DZG") in GSSI_reader.DZG_files:
+            swath_routes[swat] = GSSI_reader.get_swat_latlong(swat)
+
+
+    swath_poly = dict()
+    for item in swath_routes:
+        swath_poly[item] = list(_gps_line_to_poly(swath_routes[item]).exterior.coords)
+
+    global map_toggle
+    map_toggle = True
+    map_widget = OverzoomMapView(plot_frame, corner_radius=0, max_zoom=22)
+    map_widget.native_max_zoom = 19
+    # map_widget = tkintermapview.TkinterMapView(plot_frame, corner_radius=0, max_zoom=22)
+    # map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}", max_zoom=22)
+    # map_widget.set_tile_server(
+    #     "https://service.pdok.nl/hwh/luchtfotorgb/wmts/v1_0/2026_orthoHR/EPSG:3857/{z}/{x}/{y}.jpeg",
+    #     max_zoom=22  # try 20, 21, 22 - see note below
+    # )
+    map_widget.pack(fill=tk.BOTH, expand=True)
+
+    # Center roughly on the mock survey area and zoom in close
+    all_points = [pt for pts in swath_routes.values() for pt in pts]
+    avg_lat = sum(p[0] for p in all_points) / len(all_points)
+    avg_lon = sum(p[1] for p in all_points) / len(all_points)
+    map_widget.set_position(avg_lat, avg_lon)
+    map_widget.set_zoom(17)
+
+    update_plot_gssi()
+
+    # --- Interaction ---
+    # def toggle_cell(event):
+    #     SHIFT = 0x0001
+    #     CTRL = 0x0004
+    #
+    #     if event.state & (SHIFT | CTRL):
+    #         return
+    #
+    #     item = tree.identify_row(event.y)
+    #     column = tree.identify_column(event.x)
+    #
+    #     if not item:
+    #         return
+    #
+    #     values = list(tree.item(item, "values"))
+    #
+    #     # Checkbox column
+    #     if column == "#1":
+    #         values[0] = "☑" if values[0] == "☐" else "☐"
+    #
+    #     # Type column
+    #     elif column == "#4":
+    #         values[3] = "T" if values[3] == "L" else "L"
+    #
+    #     tree.item(item, values=values)
+    #     update_plot()
+    #
+    # tree.bind("<Button-1>", toggle_cell)
+
+    def on_tree_select(event):
+        update_plot_gssi()
+
+    tree.bind("<<TreeviewSelect>>", on_tree_select)
+
 # ---
 IDS_reader = IDS_Reader()
+GSSI_reader = GSSI_Reader()
 
 # ---- Build the window ----
 root = tk.Tk()
@@ -479,6 +703,7 @@ root.config(menu=menu_bar)
 file_menu = tk.Menu(menu_bar, tearoff=0)
 # file_menu.add_command(label="New ", command=new_file)
 file_menu.add_command(label="Open IDS", command=_open_ids)
+file_menu.add_command(label="Open GSSI", command=_open_gssi)
 # file_menu.add_command(label="Save As...", command=save_file)
 file_menu.add_separator()
 file_menu.add_command(label="Exit", command=_exit_app)
